62703395c1e59dc714f21879b053962aaaef772b
AspetereCoder <pedrinhoalgusto07@gmail.com>
2025-07-21 20:32:27 -0300

    adiciona swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle("Taskger API")
+    .setDescription("Uma API para o gerenciamentode de tarefas pessoais")
+    .setVersion("0.1")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
